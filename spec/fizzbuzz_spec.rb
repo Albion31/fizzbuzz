fe39a35faa333a0822d8dd220463257ef3d9bdd1
Cal <cal7907@hotmail.co.uk>
2017-11-13 16:48:57 +0000

    Now passes 1, add test for 2

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,7 +4,10 @@ describe 'fizzbuzz' do
     it 'returns "fizz" when passed 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
-    it 'returns "1" when passed 1' do
-        expect(fizzbuzz(1)).to eq '1'
+    it 'returns 1 when passed 1' do
+        expect(fizzbuzz(1)).to eq 1
+    end
+    it 'returns 2 when passed 2' do
+        expect(fizzbuzz(2)).to eq 2
     end
 end
