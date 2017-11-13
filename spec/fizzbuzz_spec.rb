968bc95d39baab0aac3f85e38e83aa157b9285cf
Cal <cal7907@hotmail.co.uk>
2017-11-13 16:52:58 +0000

    Passes 5, adding test for 6

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -11,6 +11,9 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(2)).to eq 2
     end
     it 'returns "buzz" when passed 5' do
-      expect(fizzbuzz(5)).to eq "buzz"
+        expect(fizzbuzz(5)).to eq "buzz"
+    end
+    it 'returns "fizz" when passed 6' do
+        expect(fizzbuzz(6)).to eq "Fizz"
     end
 end
